bc9f1f6115f1be691b8c821ce050c5ffdfe3cd9a
Trigun-123 <triguntamragouri.in>
2025-08-02 10:50:25 +0530

    typecasting

---
+++ Basics/typeCasting.java
@@ -0,0 +1,9 @@
+class typeCasting {
+    public static void main(String[] args) {
+        double num = 10.7;
+        int num1 = (int) num; // explicit typecasting
+        System.out.println(num1);
+    }
+}
+
+// final keyword -> When we add final keyword to any variable, we cannot change the value of that variable further.
