006c691c0b510282cbc7e1fd007444edbf7f6e7d
Trigun-123 <triguntamragouri.in>
2025-12-22 11:47:51 +0530

    pair

--- STL/pair.cpp
+++ STL/pair.cpp
@@ -3,12 +3,12 @@ using namespace std;
 
 int main() {
     pair<int, int> pr1 = {2, 8};
-    cout << pr1.first << " " << pr1.second << "\n";
+    cout << pr1.first << " " << pr1.second << "\n"; //2 8
 
     pair<int, char> pr2 = {2, 'b'};
-    cout << pr2.first << " " << pr2.second << "\n";
+    cout << pr2.first << " " << pr2.second << "\n"; //2 b 
 
     pair<pair<int, char>, int> pr3 = {{4, 'h'}, 8};
-    cout << pr3.first.first << " " << pr3.first.second << " " << pr3.second;
+    cout << pr3.first.first << " " << pr3.first.second << " " << pr3.second; //4 h 8
     return 0;
 }
